80def0236231dbbf0f0b7cd78686d88898171b84
dcarreno2007 <dcarreno2007@gmail.com>
2026-06-19 13:24:38 -0500

    Subir proyecto completo

--- src/main/java/com/example/SistemaJuegos/Aficionado.java
+++ src/main/java/com/example/SistemaJuegos/Aficionado.java
@@ -1,9 +1,14 @@
 package com.example.SistemaJuegos;
 
-public class Aficionado {
+public class Aficionado extends Usuario{
     private String paisFav;
     private String celular;
 
-    public void consultarPartidos(){}
+    public void consultarPartidos(){
+        System.out.println("Codigo: " + codigo_unico);
+        System.out.println("Partido: ");
+        System.out.println("Fecha: " );
+    }
+
 
 }
