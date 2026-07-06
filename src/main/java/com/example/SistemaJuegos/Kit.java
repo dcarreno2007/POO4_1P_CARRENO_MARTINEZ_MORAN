246734786b6387d26f918e0fa36b92bee17f8a13
dcarreno2007 <dcarreno2007@gmail.com>
2026-07-06 14:35:03 -0500

    Agregación de métodos getters y setters y métodos

--- src/main/java/com/example/SistemaJuegos/Kit.java
+++ src/main/java/com/example/SistemaJuegos/Kit.java
@@ -8,4 +8,43 @@ public class Kit {
     private ArrayList <Partido> partidosIncluidos;
     private double precio;
     private int disponibles;
+
+    public String getCodigo() {
+        return this.codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return this.codigo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public ArrayList getPartidosIncluidos() {
+        return this.partidosIncluidos;
+    }
+
+    public void setPartidosIncluidos(ArrayList partidosIncluidos) {
+        this.partidosIncluidos = partidosIncluidos;
+    }
+
+    public boolean validarDisponibilidad() {
+        return disponibles > 0;
+    }
+
+    public void actualizarDisponibilidad() {
+    }
 }
