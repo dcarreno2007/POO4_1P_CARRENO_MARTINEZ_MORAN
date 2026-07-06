4d36c8320113f09d5ba487664a14682fc2103603
dcarreno2007 <dcarreno2007@gmail.com>
2026-07-06 12:05:09 -0500

    Agregación de atributos clase Kit

---
+++ src/main/java/com/example/SistemaJuegos/Kit.java
@@ -0,0 +1,11 @@
+package com.example.SistemaJuegos;
+import java.util.ArrayList;
+
+public class Kit {
+    private String codigo;
+    private String nombre;
+    private String descripcion;
+    private ArrayList <Partido> partidosIncluidos;
+    private double precio;
+    private int disponibles;
+}
