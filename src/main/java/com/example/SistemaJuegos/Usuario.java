aedbe5c9855b3edba58586f4617a893eb1d472c2
dcarreno2007 <dcarreno2007@gmail.com>
2026-07-13 18:38:15 -0500

    Implementa clase abstracta Usuario y clase enum TipoUsuario

--- src/main/java/com/example/SistemaJuegos/Usuario.java
+++ src/main/java/com/example/SistemaJuegos/Usuario.java
@@ -1,12 +1,9 @@
 package com.example.SistemaJuegos;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 
 public abstract class Usuario {
-    protected String codigo_unico;
+    protected String codigoUnico;
     protected String cedula;
     protected String nombre;
     protected String apellido;
@@ -15,8 +12,9 @@ public abstract class Usuario {
     protected String correo;
     protected TipoUsuario rol;
 
-    public Usuario(String codigo_unico,String cedula, String nombre,String apellido, String usuario, String contrasena, String correo, TipoUsuario rol){
-        this.codigo_unico = codigo_unico;
+    public Usuario(String codigoUnico, String cedula, String nombre, String apellido, String usuario, String contrasena, String correo, TipoUsuario rol){
+        // Constructor para inicializar los atributos del usuario
+        this.codigoUnico = codigoUnico;
         this.cedula = cedula;
         this.nombre = nombre;
         this.apellido = apellido;
@@ -25,37 +23,66 @@ public abstract class Usuario {
         this.correo = correo;
         this.rol = rol;
     }
-    
-    public void menuOpciones(){
-        System.out.println(" 1. Consultar partidos ");
-    }
-    
-    public void consultarEntradas(){
 
-    }
-//    @Override
-//    public String toString(){}
+    // Getters y setters para los atributos del usuario
 
-    public static ArrayList<Usuario> cargarUsuarios(String ruta) {
-        ArrayList<Usuario> usuarios = new ArrayList<>();
-        try (BufferedReader bf = new BufferedReader(new FileReader(ruta))) {
-            String line;
-            line = bf.readLine();
-            while ((line = bf.readLine()) != null) {
-                String [] aficionadoL = line.split("|");
-                String CodigoUnico = aficionadoL[0];
-                String Cedula = aficionadoL[1];
-                String Nombres = aficionadoL[2];
-                String Apellidos = aficionadoL[3];
-                String Contrasena = aficionadoL[4];
-                String Usuario = aficionadoL[7];
-                String Correo = aficionadoL[8];
-                TipoUsuario rol = TipoUsuario.valueOf(aficionadoL[9]);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return usuarios;
+    public String getCodigoUnico() {
+        return codigoUnico;
+    }
+
+    public void setCodigoUnico(String codigoUnico) {
+        this.codigoUnico = codigoUnico;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public TipoUsuario getRol() {
+        return rol;
+    }
+
+    public void setRol(TipoUsuario rol) {
+        this.rol = rol;
+    }
+
+    
+    // Método abstracto para consultar entradas, que debe ser implementado por las subclases
+
+    public abstract void consultarEntradas(ArrayList<Compra> compras);
+
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "Código='" + codigoUnico + '\'' +
+                ", cédula='" + cedula + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", usuario='" + usuario + '\'' +
+                ", correo='" + correo + '\'' +
+                ", rol=" + rol +
+                '}';
     }
 
 
