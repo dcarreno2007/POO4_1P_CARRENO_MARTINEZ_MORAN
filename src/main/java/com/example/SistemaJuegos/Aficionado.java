9fc66668d791aee8a4cb8dd24f6ef4cd0dd94e6a
dcarreno2007 <dcarreno2007@gmail.com>
2026-07-13 17:41:58 -0500

    Agrega método en Aficionado

--- src/main/java/com/example/SistemaJuegos/Aficionado.java
+++ src/main/java/com/example/SistemaJuegos/Aficionado.java
@@ -1,13 +1,49 @@
 package com.example.SistemaJuegos;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 
 public class Aficionado extends Usuario{
     private String paisFav;
     private String celular;
 
+    public Aficionado(String codigo_unico, String cedula, String nombres, String apellidos, String celular, String paisFav, String usuario, String correo) {
+        super(codigo_unico, cedula, nombres, apellidos, usuario, correo, TipoUsuario.AFICIONADOS);
+        this.celular = celular;
+        this.paisFav = paisFav;
+    }
+
+    public Aficionado(String codigo_unico, String cedula, String nombres, String apellidos, String celular, String paisFav) {
+        super(codigo_unico, cedula, nombres, apellidos, null, null, TipoUsuario.AFICIONADOS);
+        this.celular = celular;
+        this.paisFav = paisFav;
+    }
+    
     public void consultarPartidos(){
         System.out.println("Codigo: " + codigo_unico);
         System.out.println("Partido: ");
         System.out.println("Fecha: " );
+    }
+
+    public static ArrayList<Aficionado> cargarAficionados(String ruta) {
+        ArrayList<Aficionado> aficionados = new ArrayList<>();
+        try (BufferedReader bf = new BufferedReader(new FileReader(ruta))) {
+            String line;
+            line = bf.readLine();
+            while ((line = bf.readLine()) != null) {
+                String [] aficionadoL = line.split("|");
+                String CodigoUnico = aficionadoL[0];
+                String Cedula = aficionadoL[1];
+                String Nombres = aficionadoL[2];
+                String Apellidos = aficionadoL[3];
+                String Celular = aficionadoL[5];
+                String PaisFavorito = aficionadoL[6];
+                aficionados.add(new Aficionado(CodigoUnico, Cedula, Nombres, Apellidos, Celular, PaisFavorito));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return aficionados;
     }
 
 
--- src/main/java/com/example/SistemaJuegos/Usuario.java
+++ src/main/java/com/example/SistemaJuegos/Usuario.java
@@ -1,22 +1,30 @@
 package com.example.SistemaJuegos;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public abstract class Usuario {
     protected String codigo_unico;
     protected String cedula;
     protected String nombre;
     protected String apellido;
     protected String usuario;
+    protected String contrasena;
     protected String correo;
     protected TipoUsuario rol;
 
-    //public Usuario(String codigo_unico,String cedula, String nombre,String apellido, String usuario, String correo, TipoUsuario rol){
-      //  this.codigo_unico = codigo_unico;
-        //this.cedula = cedula;
-       // this.nombre = nombre;
-       // this.apellido = apellido;
-       // this.usuario= usuario;
-       // this.correo=correo;
-    //}
+    public Usuario(String codigo_unico,String cedula, String nombre,String apellido, String usuario, String contrasena, String correo, TipoUsuario rol){
+        this.codigo_unico = codigo_unico;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        this.correo = correo;
+        this.rol = rol;
+    }
     
     public void menuOpciones(){
         System.out.println(" 1. Consultar partidos ");
@@ -27,6 +35,28 @@ public abstract class Usuario {
     }
 //    @Override
 //    public String toString(){}
+
+    public static ArrayList<Usuario> cargarUsuarios(String ruta) {
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+        try (BufferedReader bf = new BufferedReader(new FileReader(ruta))) {
+            String line;
+            line = bf.readLine();
+            while ((line = bf.readLine()) != null) {
+                String [] aficionadoL = line.split("|");
+                String CodigoUnico = aficionadoL[0];
+                String Cedula = aficionadoL[1];
+                String Nombres = aficionadoL[2];
+                String Apellidos = aficionadoL[3];
+                String Contrasena = aficionadoL[4];
+                String Usuario = aficionadoL[7];
+                String Correo = aficionadoL[8];
+                TipoUsuario rol = TipoUsuario.valueOf(aficionadoL[9]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return usuarios;
+    }
 
 
 }
