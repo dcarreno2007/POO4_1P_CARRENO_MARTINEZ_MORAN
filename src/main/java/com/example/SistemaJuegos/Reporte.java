9936586eb6130b7de823c62738ca58852eaaed25
JoaquinDS3 <joaquinmoran042@gmail.com>
2026-07-14 01:38:33 -0500

    Creacion y construcion de las clases Reporte y Compra

--- src/main/java/com/example/SistemaJuegos/Reporte.java
+++ src/main/java/com/example/SistemaJuegos/Reporte.java
@@ -1,20 +1,70 @@
 package com.example.SistemaJuegos;
+import java.util.Date;
 
 public class Reporte {
+    private Date fechaGen;
     private int totalCompras;
     private int totalEntradas;
     private int totalKits;
     private double montoTotal;
 
+    public Reporte(Date fechaGen, int totalCompras, int totalEntradas, int totalKits, double montoTotal) {
+        this.fechaGen = fechaGen;
+        this.totalCompras = totalCompras;
+        this.totalEntradas = totalEntradas;
+        this.totalKits = totalKits;
+        this.montoTotal = montoTotal;
+    }
 
-//Para generar el reporte, el sistema deberá recorrer la lista de compras registradas y calcular, 
-//como mínimo, la siguiente información:
-//1. Total de compras registradas.
-//2. Cantidad de compras de tipo ENTRADA.
-//3. Cantidad de compras de tipo KIT.
-//4. Monto total recaudado por ventas.
+    public String generarRessumen(){
+        String a = "Fecha de Generación: " + fechaGen + "\n" +
+                "Total de Compras: " + totalCompras + "\n" +
+                "Total de Entradas: " + totalEntradas + "\n" +
+                "Total de Kits: " + totalKits + "\n" +
+                "Monto Total: " + montoTotal;
+                return a;
+    }
+    @Override
+    public String toString() {
+        return "Reporte{" +
+                "fechaGen=" + fechaGen +
+                ", totalCompras=" + totalCompras +
+                ", totalEntradas=" + totalEntradas +
+                ", totalKits=" + totalKits +
+                ", montoTotal=" + montoTotal +
+                '}';
+    }
 
+    public Date getFechaGen() {
+        return fechaGen;
+    }
+    public int getTotalCompras() {
+        return totalCompras;
+    }
+    public int getTotalEntradas() {
+        return totalEntradas;
+    }
+    public int getTotalKits() {
+        return totalKits;
+    }
+    public double getMontoTotal() {
+        return montoTotal;
+    }
 
-    //Retorna String:
-    public void geneararResumen(){}
+    public void setFechaGen(Date fechaGen) {
+        this.fechaGen = fechaGen;
+    }
+    public void setTotalCompras(int totalCompras) {
+        this.totalCompras = totalCompras;
+    }
+    public void setTotalEntradas(int totalEntradas) {
+        this.totalEntradas = totalEntradas;
+    }
+    public void setTotalKits(int totalKits) {
+        this.totalKits = totalKits;
+    }
+    public void setMontoTotal(double montoTotal) {
+        this.montoTotal = montoTotal;
+    }
 }
+
