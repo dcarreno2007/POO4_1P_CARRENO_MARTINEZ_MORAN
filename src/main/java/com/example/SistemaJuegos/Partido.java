0148398875bba82df9796bdf356d60bcfaa2d422
JoaquinDS3 <joaquinmoran042@gmail.com>
2026-07-13 23:47:46 -0500

    Arreglo de la clase partido

--- src/main/java/com/example/SistemaJuegos/Partido.java
+++ src/main/java/com/example/SistemaJuegos/Partido.java
@@ -12,13 +12,162 @@ public class Partido {
     private int entradasPreferencial;
     private int entradasVIP;
     private String fase;
+    private static double PRECIOGENERAL = 50.0;
+    private static double PRECIOPREFERENCIAL = 100.0; 
+    private static double PRECIOVIP = 200.0; 
+
+    public Partido(String codigo, String seleccionLocal, String seleccionVisitante, String fecha, String estadio, int capacidad, String ciudad, int entradasGeneral, int entradasPreferencial, int entradasVIP, String fase) {
+        this.codigo = codigo;
+        this.seleccionLocal = seleccionLocal;
+        this.seleccionVisitante = seleccionVisitante;
+        this.fecha = fecha;
+        this.estadio = estadio;
+        this.capacidad = capacidad;
+        this.ciudad = ciudad;
+        this.entradasGeneral = entradasGeneral;
+        this.entradasPreferencial = entradasPreferencial;
+        this.entradasVIP = entradasVIP;
+        this.fase = fase;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+    public String getSeleccionLocal() {
+        return seleccionLocal;
+    }
+    public String getSeleccionVisitante() {
+        return seleccionVisitante;
+    }
+    public String getFecha() {
+        return fecha;
+    }
+    public String getEstadio() {
+        return estadio;
+    }
+    public int getCapacidad() {
+        return capacidad;
+    }
+    public String getCiudad() {
+        return ciudad;
+    }
+    public int getEntradasGeneral() {
+        return entradasGeneral;
+    }
+    public int getEntradasPreferencial() {
+        return entradasPreferencial;
+    }
+    public int getEntradasVIP() {
+        return entradasVIP;
+    }
+    public String getFase() {
+        return fase;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    public void setSeleccionLocal(String seleccionLocal) {
+        this.seleccionLocal = seleccionLocal;
+    }
+    public void setSeleccionVisitante(String seleccionVisitante) {
+        this.seleccionVisitante = seleccionVisitante;
+    }
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    public void setEstadio(String estadio) {
+        this.estadio = estadio;
+    }
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+    public void setEntradasGeneral(int entradasGeneral) {
+        this.entradasGeneral = entradasGeneral;
+    }
+    public void setEntradasPreferencial(int entradasPreferencial) {
+        this.entradasPreferencial = entradasPreferencial;
+    }
+    public void setEntradasVIP(int entradasVIP) {
+        this.entradasVIP = entradasVIP;
+    }
+    public void setFase(String fase) {
+        this.fase = fase;
+    }
+    public static double getPrecioGeneral() {
+        return PRECIOGENERAL;
+    }
+    public static double getPrecioPreferencial() {
+        return PRECIOPREFERENCIAL;
+    }
+    public static double getPrecioVIP() {
+        return PRECIOVIP;
+    }
+
+    public double obtenerPrecioEntrada(String tipoEntrada) {
+        switch (tipoEntrada.toLowerCase()) {
+            case "general":
+                return PRECIOGENERAL;
+            case "preferencial":
+                return PRECIOPREFERENCIAL;
+            case "vip":
+                return PRECIOVIP;
+            default:
+                throw new IllegalArgumentException("Tipo de entrada no válido: " + tipoEntrada);
+        }
+    }
+
+    public int ObtenerDisponibilidad(Zona zona) {
+        switch (zona) {
+            case GENERAL:
+                return entradasGeneral;
+            case PREFERENCIAL:
+                return entradasPreferencial;
+            case VIP:
+                return entradasVIP;
+            default:
+                throw new IllegalArgumentException("Zona no válida: " + zona);
+        }
+    }
 
     public boolean validarStock(Zona zona, int cantidad){
-        return true;
+        return ObtenerDisponibilidad(zona) >= cantidad && cantidad > 0;
     }
-    public void actualizarDisponibilidad (Zona zona, int cantidad){}
-    //@Override
-    //public String toString(){}
+
+    public void actualizarDisponibilidad (Zona zona, int cantidad){
+        switch (zona) {
+            case GENERAL:
+                entradasGeneral -= cantidad;
+                break;
+            case PREFERENCIAL:
+                entradasPreferencial -= cantidad;
+                break;
+            case VIP:
+                entradasVIP -= cantidad;
+                break;
+            default:
+                throw new IllegalArgumentException("Zona no válida: " + zona);
+        }
+    }
+    public String toString() {
+        return "Partido{" +
+                "codigo='" + codigo + '\'' +
+                ", seleccionLocal='" + seleccionLocal + '\'' +
+                ", seleccionVisitante='" + seleccionVisitante + '\'' +
+                ", fecha='" + fecha + '\'' +
+                ", estadio='" + estadio + '\'' +
+                ", capacidad=" + capacidad +
+                ", ciudad='" + ciudad + '\'' +
+                ", entradasGeneral=" + entradasGeneral +
+                ", entradasPreferencial=" + entradasPreferencial +
+                ", entradasVIP=" + entradasVIP +
+                ", fase='" + fase + '\'' +
+                '}';    
+
+    }
+   
     
 
 
