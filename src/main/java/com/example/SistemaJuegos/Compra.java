e9292a5dd6ca8bd4233be4fa127fc2703621f80c
JoaquinDS3 <joaquinmoran042@gmail.com>
2026-07-14 01:59:09 -0500

    Clases de compra y reporte

---
+++ src/main/java/com/example/SistemaJuegos/Compra.java
@@ -0,0 +1,101 @@
+package com.example.SistemaJuegos;
+import java.util.Date;
+
+public class Compra {
+    private static int contadorCompras = 0;
+    private String codigo;
+    private TipoCompra tipo;
+    private String codigoReferencia;
+    private Date fechaCompra;
+    private int cantidad;
+    private double valorPagado;
+    private String codigoAficionado;
+    private Zona zona;
+
+    public Compra(TipoCompra tipoCompra, int codigoRef, String fechaCompra, int cantidad, double valorPagado, int codigoAficionado, Zona zona) {
+        this.codigo = generarCodigo();
+        this.tipo = tipoCompra;
+        this.codigoReferencia = generarCodigo();
+        this.fechaCompra = new Date();
+        this.cantidad = cantidad;
+        this.valorPagado = valorPagado;
+        this.codigoAficionado = String.valueOf(codigoAficionado);
+        this.zona = zona;
+        contadorCompras++;
+    }
+    public String generarCodigo(){
+        return String.format("C%03d", contadorCompras);
+    }
+
+    @Override
+    public String toString(){
+        String zonaTexto;
+        if (zona != null) {
+            zonaTexto = zona.toString();
+        } else {
+            zonaTexto = "SIN_ZONA";
+        }
+        return codigo + "|" + tipo + "|" + codigoReferencia + "|" + fechaCompra + "|" + cantidad + "|" + valorPagado + "|" + codigoAficionado + "|" + zonaTexto;
+    }
+
+    public String getCodigo(){
+        return codigo;
+    }
+    public TipoCompra getTipo(){
+        return tipo;
+    }
+    public String getCodigoReferencia(){
+        return codigoReferencia;
+    }
+    public Date getFechaCompra(){
+        return fechaCompra;
+    }
+    public int getCantidad(){
+        return cantidad;
+    }
+    public double getValorPagado(){
+        return valorPagado;
+    }
+    public String getCodigoAficionado(){
+        return codigoAficionado;
+    }
+    public Zona getZona(){
+        return zona;
+    }
+
+    public void setTipo(TipoCompra tipo){
+        this.tipo = tipo;
+    }
+    public void setCodigo(String codigo){
+        this.codigo = codigo;
+    }
+    public void setCodigoReferencia(String codigoReferencia){
+        this.codigoReferencia = codigoReferencia;
+    }
+    public void setFechaCompra(Date fechaCompra){
+        this.fechaCompra = fechaCompra;
+    }
+    public void setCantidad(int cantidad){
+        this.cantidad = cantidad;
+    }
+    public void setValorPagado(double valorPagado){
+        this.valorPagado = valorPagado;
+    }
+    public void setCodigoAficionado(String codigoAficionado){
+        this.codigoAficionado = codigoAficionado;
+    }
+    public void setZona(Zona zona){
+        this.zona = zona;
+    }
+    public static void setContadorCompras(int valor) {
+        contadorCompras = valor;
+    }
+    public static void actualizarContadorDesdeCodigo(String codigo) {
+        int numero = Integer.parseInt(codigo.substring(1));
+        if (numero > contadorCompras) {
+            contadorCompras = numero;
+        }
+    }
+
+    
+}
---
+++ src/main/java/com/example/SistemaJuegos/Reporte.java
@@ -0,0 +1,69 @@
+package com.example.SistemaJuegos;
+import java.util.Date;
+
+public class Reporte {
+    Date fechaGen;
+    int totalCompras;
+    int totalEntradas;
+    int totalKits;
+    double montoTotal;
+
+    public Reporte(Date fechaGen, int totalCompras, int totalEntradas, int totalKits, double montoTotal) {
+        this.fechaGen = fechaGen;
+        this.totalCompras = totalCompras;
+        this.totalEntradas = totalEntradas;
+        this.totalKits = totalKits;
+        this.montoTotal = montoTotal;
+    }
+
+    public String generarRessumen(){
+        String a = "Fecha de Generación: " + fechaGen + "\n" +
+                "Total de Compras: " + totalCompras + "\n" +
+                "Total de Entradas: " + totalEntradas + "\n" +
+                "Total de Kits: " + totalKits + "\n" +
+                "Monto Total: " + montoTotal;
+                return a;
+    }
+    @Override
+    public String toString() {
+        return "Reporte{" +
+                "fechaGen=" + fechaGen +
+                ", totalCompras=" + totalCompras +
+                ", totalEntradas=" + totalEntradas +
+                ", totalKits=" + totalKits +
+                ", montoTotal=" + montoTotal +
+                '}';
+    }
+
+    public Date getFechaGen() {
+        return fechaGen;
+    }
+    public int getTotalCompras() {
+        return totalCompras;
+    }
+    public int getTotalEntradas() {
+        return totalEntradas;
+    }
+    public int getTotalKits() {
+        return totalKits;
+    }
+    public double getMontoTotal() {
+        return montoTotal;
+    }
+
+    public void setFechaGen(Date fechaGen) {
+        this.fechaGen = fechaGen;
+    }
+    public void setTotalCompras(int totalCompras) {
+        this.totalCompras = totalCompras;
+    }
+    public void setTotalEntradas(int totalEntradas) {
+        this.totalEntradas = totalEntradas;
+    }
+    public void setTotalKits(int totalKits) {
+        this.totalKits = totalKits;
+    }
+    public void setMontoTotal(double montoTotal) {
+        this.montoTotal = montoTotal;
+    }
+}
